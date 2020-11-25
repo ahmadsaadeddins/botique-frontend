7cebac3b07626c98cc0057687c8c13501dccf92e
ahmadsaadeddins <68161008+ahmadsaadeddins@users.noreply.github.com>
2020-11-25 15:11:29 +0200

    ca

---
+++ botique/src/App/App.js
@@ -0,0 +1,18 @@
+import SideMenu from "../components/SideMenu";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <SideMenu />
+
+      <header className="App-header">
+        <div>
+          <h1>asd</h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ botique/src/components/SideMenu.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./SideMenu.css";
+
+function SideMenu() {
+  return <div className="SideMenu"></div>;
+}
+
+export default SideMenu;
